659b66e476bbce3927c180c21a8776858c5139f2
141113651 <alucardhanafi21@gmail.com>
2016-11-24 15:55:25 +0700

    volleyball_problem

---
+++ Selasa_141113651_AchmadHanafi/volleyball_problem/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace volleyball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tmblHitung = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputB = new System.Windows.Forms.TextBox();
+            this.inputA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TmpyHasil = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmblHitung
+            // 
+            this.tmblHitung.Location = new System.Drawing.Point(162, 106);
+            this.tmblHitung.Name = "tmblHitung";
+            this.tmblHitung.Size = new System.Drawing.Size(75, 23);
+            this.tmblHitung.TabIndex = 13;
+            this.tmblHitung.Text = "Hitung";
+            this.tmblHitung.UseVisualStyleBackColor = true;
+            this.tmblHitung.Click += new System.EventHandler(this.tmblHitung_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Hasil";
+            // 
+            // inputB
+            // 
+            this.inputB.Location = new System.Drawing.Point(123, 71);
+            this.inputB.Name = "inputB";
+            this.inputB.Size = new System.Drawing.Size(114, 20);
+            this.inputB.TabIndex = 10;
+            // 
+            // inputA
+            // 
+            this.inputA.Location = new System.Drawing.Point(123, 26);
+            this.inputA.Name = "inputA";
+            this.inputA.Size = new System.Drawing.Size(114, 20);
+            this.inputA.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Masukkan Skor B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Masukkan Skor A";
+            // 
+            // TmpyHasil
+            // 
+            this.TmpyHasil.AutoSize = true;
+            this.TmpyHasil.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TmpyHasil.Location = new System.Drawing.Point(161, 175);
+            this.TmpyHasil.Name = "TmpyHasil";
+            this.TmpyHasil.Size = new System.Drawing.Size(0, 31);
+            this.TmpyHasil.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 279);
+            this.Controls.Add(this.TmpyHasil);
+            this.Controls.Add(this.tmblHitung);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputB);
+            this.Controls.Add(this.inputA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tmblHitung;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputB;
+        private System.Windows.Forms.TextBox inputA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TmpyHasil;
+    }
+}
+
---
+++ Selasa_141113651_AchmadHanafi/volleyball_problem/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace volleyball_problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const long mds = 1000000007;
+
+        private void tmblHitung_Click(object sender, EventArgs e)
+        {
+
+            long a, b, hasilnya;
+            a = Convert.ToInt64(inputA.Text);
+            b = Convert.ToInt64(inputB.Text);
+            if (a < b)
+            {
+                long temp = a;
+                a = b;
+                b = temp;
+            }
+
+            if ((a > 25 && a - b != 2) || (a - b < 2) || (a < 25))
+            {
+                hasilnya = 0;
+            }
+            else
+            {
+                hasilnya = campur(Math.Min(a + b - 1, 47), Math.Min(a - 1, 24));
+                hasilnya *= smds(2, a - 25);
+                hasilnya %= mds;
+            }
+            TmpyHasil.Text = hasilnya.ToString();
+        }
+
+        private long campur(long n, long r)
+        {
+            if (n < r) return 0;
+            long hasil = 1;
+            hasil *= faktorial(n);
+            hasil %= mds;
+            hasil *= InverseEuler(faktorial(n - r));
+            hasil %= mds;
+            hasil *= InverseEuler(faktorial(r));
+            hasil %= mds;
+            return hasil;
+        }
+
+        private long InverseEuler(long n)
+        {
+            return smds(n, mds - 2);
+        }
+
+        private long faktorial(long n)
+        {
+            long hasil = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                hasil *= i;
+                hasil %= mds;
+            }
+            return hasil;
+        }
+
+        private long smds(long b, long e)
+        {
+            long res = 1;
+            while (e > 0)
+            {
+                if (e % 2 == 1)
+                {
+                    res = (res * b) % mds;
+                }
+                b = (b * b) % mds;
+                e /= 2;
+            }
+            return res % mds;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
